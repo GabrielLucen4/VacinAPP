3fc81bbce37f6fdb31f5e73a287d25b31b2c3ba6
Rodrigo Lins <rodrigolins.rsa@gmail.com>
2021-04-27 00:23:29 -0300

    Alterando porta para não conflitar com o React JS

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./src/app.js');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.set('port', port);
 
 const server = http.createServer(app);
